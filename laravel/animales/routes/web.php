caecf680ff9dafe3f301194ca5f2aa740d82f251
luciaisabel21 <lucialiaisabel000@gmail.com>
2025-01-21 11:52:02 +0100

    21ene

---
+++ laravel/animales/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\AnimalController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/hello", function () {
+    return "hola";
+    });
+
+Route::get("user/{id}", function (string $id) {
+    return "hola user con id $id";
+    });
+
+
+
+//Route::resource("/animal", AnimalController::class);
