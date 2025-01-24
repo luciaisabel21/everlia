d4e7f6c458e7fe582fbdbb719f68b2d14bd9bff3
luciaisabel21 <lucialiaisabel000@gmail.com>
2025-01-24 12:38:00 +0100

    24en

---
+++ laravel/animales/database/migrations/2025_01_24_112856_create_vets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->String('name');
+            $table->String('email')->unique();
+            $table->String('phone');
+            $table->String('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vets');
+    }
+};
